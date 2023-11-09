54a43f129e49bc2dbc0cbf968ad037d4094c75f6
kdy <kkykdy4334@gmail.com>
2023-11-09 17:32:53 +0900

    fix: change root page flex div

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import Card from "@/components/main/card";
-import { eventsInProgress } from "@/mock/events";
+import Card from '@/components/main/card';
+import { eventsInProgress } from '@/mock/events';
 
 export default function Home() {
   return (
-    <div className="bg-primary flex min-h-[100vh] max-w-[100vw] flex-col items-center justify-start p-[1.6rem] pt-0">
-      <div className="text-[2.4rem] font-bold text-white my-[3.2rem]">Current Event</div>
-      {eventsInProgress.map((event) => {
-       return <Card key={event.id} eventItem={event} />; 
-      })}
+    <div className="flex min-h-[100vh] max-w-[100vw] flex-col items-center justify-start bg-primary p-[1.6rem] pt-0">
+      <div className="my-[3.2rem] text-[2.4rem] font-bold text-white">
+        Current Event
+      </div>
+      <div className="gap-24 flex flex-col items-center justify-center">
+        {eventsInProgress.map((event) => {
+          return <Card key={event.id} eventItem={event} />;
+        })}
+      </div>
     </div>
   );
 }

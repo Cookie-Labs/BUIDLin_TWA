91fdc1bef2492084de97567c5d36a315a9da7897
kdy <kkykdy4334@gmail.com>
2023-11-15 00:11:07 +0900

    feat: make event-detail page skeleton ui

--- app/event-detail/[slug]/loading.tsx
+++ app/event-detail/[slug]/loading.tsx
@@ -1,8 +1,34 @@
 export default function Loading() {
   return (
-    <div className="flex min-h-[100vh] max-w-[100vw] flex-col items-center justify-start bg-primary p-[1.6rem] pt-0">
-      <div className="my-[3.2rem] text-[2.4rem] font-bold text-white">
-        Loading
+    <div className="relative flex min-h-[100vh] max-w-[100%] flex-col items-center justify-start gap-[3.2rem] bg-primary p-[1.6rem] pt-[2.4rem]">
+      <div className="aspect-[1/0.56145] w-full rounded-[1.6rem] bg-gray14"></div>
+      <div className="flex h-auto w-full flex-col items-start justify-center gap-[1.6rem]">
+        <div className="h-[2.4rem] w-full rounded-[1.6rem] bg-gray14" />
+        <div className="flex h-auto w-full items-center justify-start gap-[1.2rem]">
+          <div className="h-[3.2rem] w-[3.2rem] rounded-circle bg-gray14" />
+          <div className="h-[1.6rem] w-[50%] rounded-[1.6rem] bg-gray14" />
+        </div>
+        <div className="h-[1.6rem] w-full rounded-[1.6rem] bg-gray14" />
+      </div>
+      <div className="flex h-auto w-full flex-col items-start justify-center gap-[2.4rem]">
+        <div className="flex h-auto w-full flex-col items-start justify-center gap-[0.8rem]">
+          <div className="h-[2rem] w-[50%] rounded-[1.6rem] bg-gray14" />
+          <div className="h-[1.6rem] w-full rounded-[1.6rem] bg-gray14" />
+        </div>
+        <div className="flex h-auto w-full flex-col items-center justify-center gap-[1.6rem]">
+          <div className="h-[6.2rem] w-full rounded-[1.6rem] bg-gray14" />
+          <div className="h-[6.2rem] w-full rounded-[1.6rem] bg-gray14" />
+        </div>
+      </div>
+      <div className="flex h-auto w-full flex-col items-start justify-center gap-[2.4rem]">
+        <div className="flex h-auto w-full flex-col items-start justify-center gap-[0.8rem]">
+          <div className="h-[2rem] w-[50%] rounded-[1.6rem] bg-gray14" />
+          <div className="h-[1.6rem] w-full rounded-[1.6rem] bg-gray14" />
+        </div>
+        <div className="flex h-auto w-full flex-col items-center justify-center gap-[1.6rem]">
+          <div className="h-[6.2rem] w-full rounded-[1.6rem] bg-gray14" />
+          <div className="h-[6.2rem] w-full rounded-[1.6rem] bg-gray14" />
+        </div>
       </div>
     </div>
   );

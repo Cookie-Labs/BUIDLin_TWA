d9f0a0689bc9a4f24b61887b077929c0c914e858
kdy <kkykdy4334@gmail.com>
2023-11-22 20:35:43 +0900

    feat: telegram-web-app init setting start

--- components/twa-layout/index.tsx
+++ components/twa-layout/index.tsx
@@ -2,6 +2,14 @@ export default function TwaLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto min-h-[100vh] max-w-[100vw] sm:min-h-[100vh] sm:max-w-[576px]">
       {children}
+      <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      <script>
+        window.Telegram.WebApp.ready();
+        window.Telegram.WebApp.expand();
+        window.Telegram.WebApp.MainButton.show();
+      </script>
+      <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+      <script>eruda.init();</script>
     </div>
   );
 }

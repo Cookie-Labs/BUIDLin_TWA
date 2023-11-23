89ea27af18c0a483dfb3bfe7d3123c3c998b3723
kdy <kkykdy4334@gmail.com>
2023-11-23 11:45:12 +0900

    test: testing @twa-dev/sdk

--- components/twa-layout/index.tsx
+++ components/twa-layout/index.tsx
@@ -1,16 +1,14 @@
+import WebApp from '@twa-dev/sdk';
+
 export default function TwaLayout({ children }: { children: React.ReactNode }) {
+  if (typeof window !== 'undefined') {
+    WebApp.ready();
+    WebApp.expand();
+  }
+
   return (
     <div className="mx-auto min-h-[100vh] max-w-[100vw] sm:min-h-[100vh] sm:max-w-[576px]">
       {children}
-      <script src="https://telegram.org/js/telegram-web-app.js"></script>
-      <script>
-        window.Telegram.WebApp.ready(); window.Telegram.WebApp.expand();
-        {/* window.Telegram.WebApp.BackButton.show();
-        window.Telegram.WebApp.BackButton.onClick(window.history.back()); */}
-        {/* window.Telegram.WebApp.MainButton.setText('Apply');
-        window.Telegram.WebApp.MainButton.show();
-        window.Telegram.WebApp.MainButton.onClick(CALLBACKMESSAGE) */}
-      </script>
       <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
       <script>eruda.init();</script>
     </div>

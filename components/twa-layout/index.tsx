856dc3e48554782497a390c048311e9a7d86f93a
kdy <kkykdy4334@gmail.com>
2023-11-23 13:01:08 +0900

    test: tma testing2

--- components/twa-layout/index.tsx
+++ components/twa-layout/index.tsx
@@ -2,6 +2,10 @@ export default function TwaLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto min-h-[100vh] max-w-[100vw] sm:min-h-[100vh] sm:max-w-[576px]">
       {children}
+      <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      <script>
+        window.Telegram.WebApp.ready(); window.Telegram.WebApp.expand();
+      </script>
       <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
       <script>eruda.init();</script>
     </div>

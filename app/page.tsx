b5e343a1dbf95a91876c1bdf693387662bdd3799
kdy <kkykdy4334@gmail.com>
2023-11-23 13:38:59 +0900

    test: tma testing4

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Card from '@/components/main/card';
 import { eventsInProgress } from '@/mock/events';
 import ScrollToTopButton from '@/components/scroll-to-top-button';
-import InitData from '@/components/twa-component/initData';
-import MainButton from '@/components/twa-component/mainButton';
+import Test from '@/components/twa-component/test';
 
 export default function HomePage() {
-  MainButton();
-
   return (
     <div className="relative flex min-h-[100vh] max-w-[100%] flex-col items-center justify-start bg-primary p-[1.6rem] pt-[3.2rem]">
       <div className="mb-[3.2rem] text-[2.4rem] font-bold text-white">
@@ -18,7 +15,7 @@ export default function HomePage() {
         })}
       </div>
       <ScrollToTopButton />
-      <div>{InitData()}</div>
+      <Test />
     </div>
   );
 }
